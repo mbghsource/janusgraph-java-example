50a884232a304c6ce501de2eb27abef6b66c61b6
Mani <manikkandanb@gmail.com>
2017-07-02 20:54:05 +0530

    add a placeholder test new file:   src/test/java/pluradj/janusgraph/example/JavaExampleTest.java

---
+++ src/test/java/pluradj/janusgraph/example/JavaExampleTest.java
@@ -0,0 +1,11 @@
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+public class JavaExampleTest {
+    @Test
+    public void testA(){
+        org.junit.Assert.assertTrue(true);
+    }
+}
+
